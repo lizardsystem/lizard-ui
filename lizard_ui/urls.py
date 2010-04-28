8973d4dc26018f56a419ef1b7a542ecbbd2e449b
reinout <reinout@vanrees.org>
2010-04-28 12:25:35 +0000

    Moved directory for (lizard) django apps

---
+++ lizard_ui/urls.py
@@ -0,0 +1,20 @@
+from django.conf.urls.defaults import *
+from django.conf import settings
+from django.contrib import admin
+
+
+admin.autodiscover()
+
+urlpatterns = patterns(
+    '',
+    (r'^admin/', include(admin.site.urls)),
+    (r'^$', 'django.views.generic.simple.direct_to_template',
+     {'template': 'lizard_ui/example.html'}),
+    )
+
+
+if settings.DEBUG:
+    # Add this also to the projects that use lizard-ui
+    urlpatterns += patterns('',
+        (r'', include('staticfiles.urls')),
+    )
