73097b65648679c1e4e3063abcb993edc656848c
jackha <jackha@gmail.com>
2010-08-25 15:03:17 +0000

    remove unnecessary stuff

--- lizard_ui/templatetags/login.py
+++ lizard_ui/templatetags/login.py
@@ -6,7 +6,5 @@ register = template.Library()
 
 @register.inclusion_tag("lizard_ui/tag_login.html",
                         takes_context=True)
-def display_login(context, redirect_path=None):
-    if redirect_path is None:
-        redirect_path = "./"
-    return {'user': context['user'], 'redirect_path': redirect_path}
+def display_login(context):
+    return {'user': context['user']}
