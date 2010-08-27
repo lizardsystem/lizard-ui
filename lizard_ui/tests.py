e17275e010fac833d99d3b18cb0aca4e45a3c87a
reinout <reinout@vanrees.org>
2010-08-27 15:50:30 +0000

    Tests added for login/logout

--- lizard_ui/tests.py
+++ lizard_ui/tests.py
@@ -1,11 +1,44 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
 
 
-class TestHomepage(TestCase):
+class TestLoginLogout(TestCase):
 
-    def test_loginpage(self):
+    def setUp(self):
+        """Create sample user."""
+        User.objects.create_user('atilla',
+                                 'atilla@thehun.mn',
+                                 password='horses')
+
+    def test_smoke(self):
         # No parameters.
         self.assertRaises(
             KeyError,  # MultiValueDictKeyError
             self.client.get,
             '/accounts/login/')
+
+    def test_invalid_user(self):
+        response = self.client.post('/accounts/login/',
+                                    {'username': 'rianne',
+                                     'password': 'pony'})
+        # The response is always 200.
+        self.assertEquals(response.status_code, 200)
+        # But the value is the json dump of False.
+        self.assertEquals(response.content, 'false')
+
+    def test_valid_user(self):
+        response = self.client.post('/accounts/login/',
+                                    {'username': 'atilla',
+                                     'password': 'horses'})
+        # The response is always 200.
+        self.assertEquals(response.status_code, 200)
+        # But the value is the json dump of False.
+        self.assertEquals(response.content, 'true')
+
+    def test_logout(self):
+        self.client.login(username='atilla', password='horses')
+        response = self.client.get('/accounts/logout/')
+        # The response is always 200.
+        self.assertEquals(response.status_code, 200)
+        # And empty.
+        self.assertEquals(response.content, '')
