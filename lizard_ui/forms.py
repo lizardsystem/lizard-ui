4a5b356af6de15bfb4783019d8677d9ff730a8a1
reinout <reinout@vanrees.org>
2011-09-16 09:40:46 +0000

    Fixed form field: using label= instead of just passing a string.

--- lizard_ui/forms.py
+++ lizard_ui/forms.py
@@ -3,13 +3,16 @@ from django.utils.translation import ugettext as _
 
 
 class LoginForm(forms.Form):
-    username = forms.CharField(_('Username'),
-                               max_length=100,
-                               required=True)
-    password = forms.CharField(_('Password'),
-                               widget=forms.PasswordInput(),
-                               max_length=100,
-                               required=True)
-    next = forms.CharField(max_length=100,
-                           required=False,
-                           widget=forms.HiddenInput())
+    username = forms.CharField(
+        max_length=100,
+        label=_('Username'),
+        required=True)
+    password = forms.CharField(
+        max_length=100,
+        label=_('Password'),
+        widget=forms.PasswordInput(),
+        required=True)
+    next = forms.CharField(
+        max_length=100,
+        required=False,
+        widget=forms.HiddenInput())
