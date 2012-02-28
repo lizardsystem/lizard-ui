ed5113031a4c978c66f161f2c773b55fc075b84c
Reinout van Rees <reinout@vanrees.org>
2012-02-28 03:20:51 +0100

    Allowing north popups

--- lizard_ui/static/lizard_ui/lizardui.js
+++ lizard_ui/static/lizard_ui/lizardui.js
@@ -2,6 +2,7 @@ function setUpPopovers() {
   /* Everything with a matching class gets bootstrap's popover enabled.
    */
   $(".has_popover").popover();
+  $(".has_popover_north").popover({placement: 'top'});
 }
 
 
