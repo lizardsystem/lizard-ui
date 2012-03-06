bfbfa19ce5d26187fc8f1868217af31294572813
Gijs Nijholt <gijs.nijholt@gmail.com>
2012-03-06 15:52:58 +0100

    Adding collapsable sidebar.

--- lizard_ui/static/lizard_ui/lizardui.coffee
+++ lizard_ui/static/lizard_ui/lizardui.coffee
@@ -1,19 +1,46 @@
 setUpPopovers = ->
     # Everything with a matching class gets bootstrap's popover enabled.
     $(".has_popover").popover()
-    $(".has_popover_north").popover({placement: 'top'})
+    $(".has_popover_north").popover
+        placement: 'top'
     @
 
+closeSidebar = ->
+    $('.icon-arrow-left')
+        .removeClass('icon-arrow-left')
+        .addClass('icon-arrow-right')
+    $('div#sidebar').width(0)
+    $('div#content').css("left", "0px")
+    setUpMapDimensions()
+
+openSidebar = ->
+    $('.icon-arrow-right')
+        .removeClass('icon-arrow-right')
+        .addClass('icon-arrow-left')
+    $('div#sidebar').width(300)
+    $('div#content').css("left", "300px")
+    setUpMapDimensions()
 
 setUpMapDimensions = ->
-    console.log "setUpMapDimensions"
     contentHeight = $("div#content").height()
+    contentWidth = $("div#content").width()
     $("#map").height contentHeight
+    $("#map").width contentWidth
     @
 
 $(document).ready ->
+    window.sidebarState = "opened"
     setUpPopovers()
     setUpMapDimensions()
+    
+    $('.collapse-sidebar').click (e) ->
+        e.preventDefault()
+        if window.sidebarState is "opened"
+            closeSidebar()
+            window.sidebarState = "closed"
+        else
+            openSidebar()
+            window.sidebarState = "opened"
     @
 
  
--- lizard_ui/static/lizard_ui/lizardui.js
+++ lizard_ui/static/lizard_ui/lizardui.js
@@ -1,5 +1,5 @@
 (function() {
-  var setUpMapDimensions, setUpPopovers;
+  var closeSidebar, openSidebar, setUpMapDimensions, setUpPopovers;
 
   setUpPopovers = function() {
     $(".has_popover").popover();
@@ -9,17 +9,43 @@
     return this;
   };
 
+  closeSidebar = function() {
+    $('.icon-arrow-left').removeClass('icon-arrow-left').addClass('icon-arrow-right');
+    $('div#sidebar').width(0);
+    $('div#content').css("left", "0px");
+    return setUpMapDimensions();
+  };
+
+  openSidebar = function() {
+    $('.icon-arrow-right').removeClass('icon-arrow-right').addClass('icon-arrow-left');
+    $('div#sidebar').width(300);
+    $('div#content').css("left", "300px");
+    return setUpMapDimensions();
+  };
+
   setUpMapDimensions = function() {
-    var contentHeight;
-    console.log("setUpMapDimensions");
+    var contentHeight, contentWidth;
     contentHeight = $("div#content").height();
+    contentWidth = $("div#content").width();
     $("#map").height(contentHeight);
+    $("#map").width(contentWidth);
     return this;
   };
 
   $(document).ready(function() {
+    window.sidebarState = "opened";
     setUpPopovers();
     setUpMapDimensions();
+    $('.collapse-sidebar').click(function(e) {
+      e.preventDefault();
+      if (window.sidebarState === "opened") {
+        closeSidebar();
+        return window.sidebarState = "closed";
+      } else {
+        openSidebar();
+        return window.sidebarState = "opened";
+      }
+    });
     return this;
   });
 
