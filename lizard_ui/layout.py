e6069d3ff5658c1d2212685a6042e707d842e2e7
Roland van Laar <roland@micite.net>
2012-03-08 10:17:36 +0100

    Added interface class for workspace acceptable with to_html function to render the class.

--- lizard_ui/layout.py
+++ lizard_ui/layout.py
@@ -1,5 +1,8 @@
 """Layout helpers."""
 
+from django.utils.safestring import mark_safe
+from django.template import Context, loader
+
 
 class Action(object):
     """Small wrapper for actions, just to make the attributes explicit.
@@ -27,3 +30,34 @@ class Action(object):
         self.url = url
         self.description = description
         self.klass = klass
+
+
+class WorkspaceAcceptable(object):
+    """Wrapper/Interface for WorkspaceAcceptable items and html generation.
+
+       A dictionary with keys instead of attributes is just as fine for the
+    templates, but for documentation purposes this class is handier.
+
+    - **name**: text of the link.
+
+    """
+    template = 'lizard_ui/workspace_acceptable.html'
+
+    def __init__(self, name, description, adapter_layer_json, url=None,
+                 workspace_type=None, adapter_name=None):
+        self.name = name
+        self.description = description
+        self.adapter_layer_json = adapter_layer_json
+        self.url = url
+        if workspace_type is None:
+            self.workspace_type = 'workspace-acceptable'
+        else:
+            self.workspace_type = workspace_type
+        self.adapter_name = None
+        if adapter_name is not None:
+            self.adapter_name = adapter_name
+
+    def to_html(self):
+        template = loader.get_template(self.template)
+        context = Context({'acceptable': self})
+        return mark_safe(template.render(context))
