df9e9a301391d00b54c225fb355de05cd1ac6eef
Reinout van Rees <reinout@vanrees.org>
2012-05-18 12:01:54 +0200

    Enabled traceback logging middleware

--- lizard_ui/testsettings.py
+++ lizard_ui/testsettings.py
@@ -34,6 +34,7 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
+    'lizard_ui.middleware.TracebackLoggingMiddleware',
     'lizard_security.middleware.SecurityMiddleware',
     'tls.TLSRequestMiddleware',
     )
