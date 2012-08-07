a9da83bc24fc27ebf116fee4849d8dcc7493aaf1
ejnens <erikjan.vos@nelen-schuurmans.nl>
2012-08-07 10:59:53 +0200

    flot: calculate tick sizes

--- lizard_ui/static/lizard_ui/lizard.js
+++ lizard_ui/static/lizard_ui/lizard.js
@@ -145,7 +145,7 @@ function flotGraphLoadData($graph, max_image_width, response) {
     var plot;
     var data = response.data;
     
-    var options = {
+    var baseOptions = {
         series: {
             points: { show: true, hoverable: true }
         },
@@ -162,7 +162,6 @@ function flotGraphLoadData($graph, max_image_width, response) {
         },
         xaxis: {
             mode: "time",
-            tickSize: [2, "day"],
             axisLabel: response.x_label, // plugin jquery.flot.axislabels.js
             axisLabelUseCanvas: true,
             axisLabelFontFamily: 'Verdana,Arial,sans-serif',
@@ -172,52 +171,51 @@ function flotGraphLoadData($graph, max_image_width, response) {
         grid: { hoverable: true }
     };
 
+    var calcTickSizeAndPlot = function (x_min, x_max) {
+        var tick_size = [];
+        var diff_time = x_max - x_min;
+        var diff_seconds = diff_time/1000;
+        var diff_minutes = diff_time/1000/60;
+        var diff_hours = diff_time/1000/60/60;
+        // TODO get min time stap from timeseries
+        if (diff_hours > 24*30) {
+            $.merge(tick_size, [1, "month"]);
+        } else if (diff_hours > 24) {
+            $.merge(tick_size, [1, "day"]);
+        } else if (diff_hours > 1) {
+            $.merge(tick_size, [1, "hour"]);
+        } else if (diff_minutes > 45) {
+            $.merge(tick_size, [15, "minute"]);
+        } else if (diff_minutes > 10) {
+            $.merge(tick_size, [10, "minute"]);
+        } else if (diff_minutes > 5) {
+            $.merge(tick_size, [5, "minute"]);
+        } else if (diff_minutes > 1) {
+            $.merge(tick_size, [1, "minute"]);
+        } else if (diff_seconds > 45 ) {
+            $.merge(tick_size, [15, "second"]);
+        } else if (diff_seconds > 10) {
+            $.merge(tick_size, [10, "second"]);
+        } else {
+            $.merge(tick_size, [1, "second"]);
+        }
+        var options = $.extend(true, {}, baseOptions, {
+            xaxis: {
+                min: x_min,
+                max: x_max,
+                tickSize: tick_size
+            }
+        });
+        plot = $.plot($graph, data, options);
+    };
+
+    var rePlot = function () {
+        calcTickSizeAndPlot(response.x_min, response.x_max);
+    };
+
     $graph.bind("plotselected", function (event, ranges) {
         //$("#selection").text(ranges.xaxis.from.toFixed(1) + " to " + ranges.xaxis.to.toFixed(1));
-        var zoom = true;
-        if (zoom) {
-            var x_min_zoom, x_max_zoom, tick_size, diff_time, diff_seconds;
-            x_min_zoom = ranges.xaxis.from;
-            x_max_zoom = ranges.xaxis.to; 
-            tick_size = [];
-            diff_time = x_max_zoom - x_min_zoom;
-            diff_seconds = diff_time/1000;
-            diff_minutes = diff_time/1000/60;
-            diff_hours = diff_time/1000/60/60;
-            //TODO get min time stap from timeseries
-            if (diff_hours > 24*30) {
-                $.merge(tick_size, [1, "month"]); 
-            } else if (diff_hours > 24) {
-                $.merge(tick_size, [1, "day"]); 
-            } else if (diff_hours > 1) {
-                $.merge(tick_size, [1, "hour"]);
-            } else if (diff_minutes > 45) {
-                $.merge(tick_size, [15, "minute"]);
-            } else if (diff_minutes > 10) {
-                $.merge(tick_size, [10, "minute"]);
-            } else if (diff_minutes > 5) {
-                $.merge(tick_size, [5, "minute"]);
-            } else if (diff_minutes > 1) {
-                $.merge(tick_size, [1, "minute"]);
-            } else if (diff_seconds > 45 ) {
-                $.merge(tick_size, [15, "second"]);
-            } else if (diff_seconds > 10) {
-                $.merge(tick_size, [10, "second"]);
-            } else {
-                $.merge(tick_size, [1, "second"]);
-            }
-            plot = $.plot(
-                $graph,
-                data,
-                $.extend(true, {}, options, {
-                    xaxis: {
-                        min: ranges.xaxis.from,
-                        max: ranges.xaxis.to,
-                        tickSize: tick_size
-                    }
-                })
-            );
-        }
+        calcTickSizeAndPlot(ranges.xaxis.from, ranges.xaxis.to);
     });
 
     //$graph.bind("plotunselected", function (event) {
@@ -243,15 +241,16 @@ function flotGraphLoadData($graph, max_image_width, response) {
         //$("#y").text(pos.y.toFixed(2));
         if (item) {
             $("#charttooltip").remove();
-            var x = item.datapoint[0].toFixed(2),
-                y = item.datapoint[1].toFixed(2);
+            //var x = item.datapoint[0].toFixed(2);
+            //var y = item.datapoint[1].toFixed(2);
             showChartTooltip(item.pageX, item.pageY, item.datapoint[1]);
         } else {
             $("#charttooltip").remove();
         }
     });
 
-    plot = $.plot($graph, data, options);
+    // initial plot
+    rePlot();
 
     //$("#clearSelection").click(function () {
     //    plot.clearSelection();
@@ -262,7 +261,7 @@ function flotGraphLoadData($graph, max_image_width, response) {
     //});
 
     $(".flot-graph-reload").click(function () {
-        plot = $.plot($graph, data, options);
+        rePlot();
     });
 }
 
