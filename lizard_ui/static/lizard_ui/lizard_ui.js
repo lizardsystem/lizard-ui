160889ef8c934730aabfa976ef495aa67b567f11
ejnens <erikjan.vos@nelen-schuurmans.nl>
2013-05-21 16:45:10 +0200

    rm unused $.deparam inline; fix sidebar 1-off pixel

--- lizard_ui/static/lizard_ui/lizard_ui.js
+++ lizard_ui/static/lizard_ui/lizard_ui.js
@@ -63,14 +63,6 @@ jQuery.fn.exists = function () {
     return this.length !== 0;
 };
 
-// jQuery Deparam - v0.1.0 - 6/14/2011
-// http://benalman.com/
-// Copyright (c) 2011 Ben Alman; Licensed MIT, GPL
-(function($){var a,b=decodeURIComponent,c=$.deparam=function(a,d){var e={};$.each(a.replace(/\+/g," ").split("&"),function(a,f){var g=f.split("="),h=b(g[0]);if(!!h){var i=b(g[1]||""),j=h.split("]["),k=j.length-1,l=0,m=e;j[0].indexOf("[")>=0&&/\]$/.test(j[k])?(j[k]=j[k].replace(/\]$/,""),j=j.shift().split("[").concat(j),k++):k=0,$.isFunction(d)?i=d(h,i):d&&(i=c.reviver(h,i));if(k)for(;l<=k;l++)h=j[l]!==""?j[l]:m.length,l<k?m=m[h]=m[h]||(isNaN(j[l+1])?{}:[]):m[h]=i;else $.isArray(e[h])?e[h].push(i):h in e?e[h]=[e[h],i]:e[h]=i}});return e};c.reviver=function(b,c){var d={"true":!0,"false":!1,"null":null,"undefined":a};return+c+""===c?+c:c in d?d[c]:c}})(jQuery);
-// ^^^ TODO: this gives 11 jshint errors.
-// And *what* does it do?
-
-
 // some class aliases for Bootstrap information popovers
 var setUpPopovers = function() {
   var animation = false;
@@ -152,7 +144,7 @@ var setSidebarState = function(collapse, preventAnim) {
         // slide the content
         $('#content').animate(
             {
-                left : 300
+                left : 301
             },
             animationSpeed,
             function() {
