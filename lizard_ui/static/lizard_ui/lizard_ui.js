ae2435531e2f958e51f485514ee69a2c3490d010
ejnens <erikjan.vos@nelen-schuurmans.nl>
2013-05-23 12:29:56 +0200

    prevent FOUC when a side/right/sidebar-inner bar is hidden from the start

--- lizard_ui/static/lizard_ui/lizard_ui.js
+++ lizard_ui/static/lizard_ui/lizard_ui.js
@@ -100,9 +100,7 @@ var setSidebarState = function(collapse, preventAnim) {
         }
 
         // update arrow icon on button
-        $('.collapse-sidebar i')
-            .removeClass('icon-arrow-left')
-            .addClass('icon-arrow-right');
+        $('.collapse-sidebar i').removeClass('icon-arrow-left').addClass('icon-arrow-right');
         // disable the seconday sidebar button
         $('.secondary-sidebar-button').attr('disabled', '');
         // slide the sidebar
@@ -110,7 +108,11 @@ var setSidebarState = function(collapse, preventAnim) {
             {
                 left: -300
             },
-            animationSpeed
+            animationSpeed,
+            'swing',
+            function () {
+                $('#sidebar').hide();
+            }
         );
         // slide the content
         $('#content').animate(
@@ -118,6 +120,7 @@ var setSidebarState = function(collapse, preventAnim) {
                 left: 0
             },
             animationSpeed,
+            'swing',
             function() {
                 if (typeof map !== 'undefined') {
                     if (typeof map.updateSize !== 'undefined') {
@@ -131,24 +134,25 @@ var setSidebarState = function(collapse, preventAnim) {
     else {
         // opening
         // update arrow icon on button
-        $('.collapse-sidebar i')
-            .removeClass('icon-arrow-right')
-            .addClass('icon-arrow-left');
+        $('.collapse-sidebar i').removeClass('icon-arrow-right').addClass('icon-arrow-left');
         // enable the seconday sidebar button
         $('.secondary-sidebar-button').removeAttr('disabled');
         // slide the sidebar
+        $('#sidebar').show();
         $('#sidebar').animate(
             {
-                left : 0
+                left: 0
             },
-            animationSpeed
+            animationSpeed,
+            'swing'
         );
         // slide the content
         $('#content').animate(
             {
-                left : 301
+                left: 301
             },
             animationSpeed,
+            'swing',
             function() {
                 if (typeof map !== 'undefined') {
                     if (typeof map.updateSize !== 'undefined') {
@@ -167,45 +171,43 @@ var setSidebarState = function(collapse, preventAnim) {
 var setRightbarState = function(collapse, preventAnim) {
     var animationSpeed = preventAnim === true ? 0 : defaultAnimationSpeed;
     if (collapse) {
-        // disabled: can't seem to find secondary-rightbar anywhere?
-        // var secondaryCollapsed = $('#secondary-rightbar').data('collapsed');
-        // if (secondaryCollapsed) {
-            // hideSecondaryRightbar();
-        // }
         // collapsing
-        $('.collapse-rightbar i')
-            .removeClass('icon-arrow-right')
-            .addClass('icon-arrow-left');
+        $('.collapse-rightbar i').removeClass('icon-arrow-right').addClass('icon-arrow-left');
         $('#rightbar').animate(
             {
-                right : -251
+                right: -251
             },
-            animationSpeed
+            animationSpeed,
+            'swing',
+            function () {
+                $('#rightbar').hide();
+            }
         );
         $('#content').animate(
             {
-                right : 0
+                right: 0
             },
-            animationSpeed
+            animationSpeed,
+            'swing'
         );
     }
     else {
         // opening
-        $('.collapse-rightbar i')
-            .removeClass('icon-arrow-left')
-            .addClass('icon-arrow-right');
+        $('.collapse-rightbar i').removeClass('icon-arrow-left').addClass('icon-arrow-right');
         $('#rightbar').show();
         $('#rightbar').animate(
             {
-                right : 0
+                right: 0
             },
-            animationSpeed
+            animationSpeed,
+            'swing'
         );
         $('#content').animate(
             {
-                right : 251
+                right:251
             },
-            animationSpeed
+            animationSpeed,
+            'swing'
         );
     }
     // update the state
@@ -223,9 +225,13 @@ var setSecondarySidebarState = function(collapse, preventAnim) {
         element.css("overflow-y", "hidden");
         $("#secondary-sidebar").animate(
             {
-                top : bottom
+                top: bottom
             },
-            animationSpeed
+            animationSpeed,
+            'swing',
+            function () {
+                element.hide();
+            }
         );
         $('.secondary-sidebar-button').removeClass('active');
     }
@@ -239,9 +245,10 @@ var setSecondarySidebarState = function(collapse, preventAnim) {
         element.show();
         element.animate(
             {
-                top : top
+                top: top
             },
             animationSpeed,
+            'swing',
             function() {
                 element.css('overflow-y', 'auto');
             }
