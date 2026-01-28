edce6d7c2742d26f46d6c4cc47a0b0f8fb97d649
elisalle <eli.salle@nelen-schuurmans.nl>
2026-01-28 14:26:39 +0100

    support django 5

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ long_description = "\n\n".join(
 install_requires = (
     [
         "beautifulsoup4",
-        "Django >= 3.2, < 5",
+        "Django >= 3.2, < 6",
         "django_compressor >= 1.1",  # Yes, underscore.
     ],
 )
